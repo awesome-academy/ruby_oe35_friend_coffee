79f73faadd7934fb95848a9126780d95b85ba8f9
sub <contact.sub22@gmail.com>
2021-06-17 17:33:36 +0700

    task#37357 signup_basic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   has_many :orders, dependent: :destroy
 
+  USER_PERMIT = %i(name email password password_confirmation).freeze
+
   validates :name, presence: true,
     length: {maximum: Settings.user.validation.name_max}
   VALID_EMAIL_REGEX = Settings.user.format.email
   validates :email, presence: true,
     length: {maximum: Settings.user.validation.email_max},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  has_secure_password
+
+  validates :password, presence: true,
+    length: {minimum: Settings.user.validation.pass_min}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "style_pages#home"
+    get "/friendcoff", to: "main_pages#home"
+    get "/about", to: "main_pages#about"
+    get "/signup", to: "users#new"
+    resources :users, only: %i(show create new)
   end
 end
