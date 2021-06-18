c659056aaac38dcf3597aadebb3d8c7dd10daa83
sub <contact.sub22@gmail.com>
2021-06-18 09:58:04 +0700

    task#37356 signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
     root "style_pages#home"
     get "/friendcoff", to: "main_pages#home"
     get "/about", to: "main_pages#about"
+
     get "/signup", to: "users#new"
-    resources :users, only: %i(show create new)
+
+    get "/signin", to: "sessions#new"
+    post "/signin", to: "sessions#create"
+    delete "/signout", to: "sessions#destroy"
+
+    resources :users, only: %i(new create show)
   end
 end
